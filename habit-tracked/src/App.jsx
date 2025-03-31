15075a5717e0ee743aca0247cc0b087a75ad8158
Anthony Pecoraro <84484268+PecoraroAnthony@users.noreply.github.com>
2025-03-30 23:13:14 -0700

    Start Register/Login/Home Development

---
+++ habit-tracked/src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <>
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ habit-tracked/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+  )
+}
